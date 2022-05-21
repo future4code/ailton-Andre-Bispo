919cf210415f3099d227b737db542a3f92c12d92
André Luiz Amaral de Oliveira Bispo <aluiz.x6@gmail.com>
2022-05-20 23:15:28 -0300

    blackjack

---
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -0,0 +1,24 @@
+function blackjack (){//inicio do blackjack
+console.log ("Boas vindas ao jogo de Blackjack!")
+let comecar = confirm ("Vamos Jogar?")
+
+if(comecar === false ) {
+console.log("O jogo acabou")	
+ }
+else if (comecar === true) {
+   com
+
+   maoUser = []
+   maoPC = []
+
+   console.log ( )//`${jogador(carta1,carta2)},${pc(carta3,carta4)}`)
+
+//    if (user > pc ) && (user <= 21) {
+//       console.log("Jogador venceu!")
+//    }
+   
+//    else if (pc > user ) && (pc <= 21) {
+//       console.log("Jogador venceu!")
+//    }
+}
+}//fim do blackjack
