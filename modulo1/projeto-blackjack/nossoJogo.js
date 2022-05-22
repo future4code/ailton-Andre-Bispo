ac768a2ae853e6c24f9b1933a0784799f1a4a8f8
André Luiz Amaral de Oliveira Bispo <aluiz.x6@gmail.com>
2022-05-22 18:14:46 -0300

    arredondando

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -1,117 +1,86 @@
 
 let qualPegou
-
 let somaComputador = 0
-let deckComputador
-
-let deckJogador
+let maoComputador
+let maoJogador
 let somaJogador = 0
-
 let desejaComprar
 let somatorioTotal
-
 let podeComprar = true
 let podeComprarPc = false
 
    function jogo () {
       console.log ('Bem vindo ao jogo de Blackjack')
       let querJogar = confirm('Deseja iniciar uma nova rodada?')
-       // Caso a resposta seja positiva inicio o if
       if(querJogar === true) {
-          // Declaro os decks como array vazio
 
-         deckComputador = []
-         deckJogador = []
+        maoComputador = []
+        maoJogador = []
 
-         // Crio uma estrutura de repetição para sortear 2 cartas para cada deck
-
-         sorteiaCartas(deckComputador, 2)
-         sorteiaCartas(deckJogador, 2)
+         sorteiaCartas(maoComputador, 2)
+         sorteiaCartas(maoJogador, 2)
          checaAses()
 
-         somatorioTotalJogador()
+         pontosTotalJogador()
 
          if (somaJogador === 21) {
             podeComprar = false
          }  
 
-         // Imprimo no console os valores das mãos
-
          imprimeValorInicial()
-
-         // Somo a pontuação
-
-         somatorioTotalJogador()
-         somatorioTotalComputador()
-
-         // Pergunto se o jogador deseja comprar mais cartas
-
+         pontosTotalJogador()
+         pontosTotalComputador()
          perguntaDesejaComprarMais()
-
-         // Após o jogador comprar o computador pode comprar
-
          pcCompraMais()
-
          comparativo(somaJogador, somaComputador)
       }
       else {
          console.log('O jogo acabou')
-      }
-      }
+      }}
 
-   function checaAses (jogador, computador) {
-      while ((deckComputador[0].texto.includes("A") && deckComputador[1].texto.includes("A")) || (deckJogador[0].texto.includes("A") && deckJogador[1].texto.includes("A")))
+   function checaAses () {
+      while ((maoComputador[0].texto.includes("A") &&maoComputador[1].texto.includes("A"))
+       || (maoJogador[0].texto.includes("A") &&maoJogador[1].texto.includes("A")))
       {
          console.log('Mulligan)')
          for(let i=0; i<2; i++){
-            deckJogador.pop()                  
+           maoJogador.pop()                  
          }                 
          for(let i=0; i<2; i++){
-            deckComputador.pop()
+           maoComputador.pop()
          }
-         sorteiaCartas(deckComputador, 2)
-         sorteiaCartas(deckJogador, 2)
-         }
-         }  
-
-
-// Funcao que imprime o valor inicial
+         sorteiaCartas(maoComputador, 2)
+         sorteiaCartas(maoJogador, 2)
+         }}  
 
 function imprimeValorInicial () {
-   console.log(`A carta revelada do computador é: ${deckComputador[0].texto}`)
-   console.log(deckComputador)
-   console.log(`Usuario - cartas: ${deckJogador[0].texto} ${deckJogador[1].texto} - pontuação ${deckJogador[0].valor + deckJogador[1].valor}`)
+   console.log(`A carta revelada do computador é: ${maoComputador[0].texto}`)
+ //  console.log(maoComputador)
+   console.log(`As suas cartão são: ${maoJogador[0].texto} ${maoJogador[1].texto} -
+    pontuação ${maoJogador[0].valor +maoJogador[1].valor}`)
 }
 
-// Funcao de comprar cartas
-
-function sorteiaCartas (qualDeck, numeroDeVez) {
+function sorteiaCartas (pegarCartas, numeroDeVez) {
    for (let i = 0; i<numeroDeVez; i++){
-      qualDeck.push(comprarCarta())
+      pegarCartas.push(comprarCarta())
       }
 }
 
-// Soma o valor do jogadore
-
-function somatorioTotalJogador () {
+function pontosTotalJogador () {
    somaJogador = 0
-   for (let i = 0; i < deckJogador.length; i++){
-      somaJogador = somaJogador + deckJogador[i].valor
+   for (let i = 0; i < maoJogador.length; i++){
+      somaJogador = somaJogador + maoJogador[i].valor
    }
 }
 
-// Soma o valor do computador
-
-function somatorioTotalComputador () {
+function pontosTotalComputador () {
    somaComputador = 0
-   for (let i = 0; i < deckComputador.length; i++) {
-      somaComputador = somaComputador + deckComputador[i].valor
+   for (let i = 0; i < maoComputador.length; i++) {
+      somaComputador = somaComputador + maoComputador[i].valor
    }
 }
 
-// Função que compara
-
-function comparativo (jogado, computado) {
+function comparativo () {
 
    if ((somaJogador === 21 && somaComputador !== 21 || somaJogador < 21 && somaComputador > 21 || somaJogador < 21 && somaComputador < somaJogador)){
       console.log('O usuario venceu!')
@@ -122,29 +91,24 @@ function comparativo (jogado, computado) {
    else if ((somaComputador === somaJogador || somaComputador && somaJogador > 21)) {
       console.log('Empate!')
    }
-   console.log(somaJogador, somaComputador)  
+   console.log(`Usuario:${somaJogador}, PC: ${somaComputador}`) 
 }
-
-// Função que pergunta se o usuario deseja comprar mais
 
 function perguntaDesejaComprarMais () {
 
    while (podeComprar === true) {
       if (confirm('Deseja comprar mais cartas?')) {
-         sorteiaCartas(deckJogador, 1)
-         console.log(`Comprou ${deckJogador[deckJogador.length-1].texto}`)
-         somatorioTotalJogador()
+         sorteiaCartas(maoJogador, 1)
+         console.log(`Comprou ${maoJogador[maoJogador.length-1].texto}`)
+         pontosTotalJogador()
          if (somaJogador >= 21) {
             podeComprar = false
          }
       } else {
          podeComprar = false
       }
-
    }
 }
-
-// Função que faz o pc comprar
 
 function pcCompraMais () {
    if (somaComputador<somaJogador && somaJogador <= 21) {
@@ -152,9 +116,9 @@ function pcCompraMais () {
    }
    while (podeComprarPc === true){
 
-      sorteiaCartas(deckComputador, 1)
-      console.log(`O computador comprou ${deckComputador[deckComputador.length-1].texto}`)
-      somatorioTotalComputador()
+      sorteiaCartas(maoComputador, 1)
+      console.log(`O computador comprou ${maoComputador[maoComputador.length-1].texto}`)
+      pontosTotalComputador()
 
       if (somaComputador === 21) {
          podeComprarPc = false
