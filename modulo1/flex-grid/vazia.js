e7ad93b69aa8efda5161a0f650b1bfefa4e66a15
André Luiz Amaral de Oliveira Bispo <aluiz.x6@gmail.com>
2022-05-31 04:36:25 -0300

    nem ta tarde

---
+++ modulo1/flex-grid/vazia.js
@@ -0,0 +1,6 @@
+function contaOcorrencias(arrayDeNumeros, numeroEscolhido) {
+    let contagem = arrayDeNumeros.filter((numero) => {return numero === numeroEscolhido})
+    if (contagem.length>0){
+    return(`O número ${numeroEscolhido} aparece ${contagem.length}x`)  
+    }else {return "Número não encontrado"}
+}
