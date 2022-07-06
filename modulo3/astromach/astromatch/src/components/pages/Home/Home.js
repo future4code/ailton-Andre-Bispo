212f5e8171180346d6849669336b8bb342ba4953
André Luiz Amaral de Oliveira Bispo <aluiz.x6@gmail.com>
2022-07-06 20:55:22 -0300

    esqueleto

---
+++ modulo3/astromach/astromatch/src/components/pages/Home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div>perfil
+        <button>match</button>
+        <button>deni</button>
+    </div>
+  )
+}
---
+++ modulo3/astromach/astromatch/src/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import {createGlobalStyle} from 'styled-components';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+const GlobalStyle = createGlobalStyle`*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+`
+export default GlobalStyle
+
