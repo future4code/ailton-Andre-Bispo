a246e21acd123c79496ef25a22a1695a1fd972e5
André Luiz Amaral de Oliveira Bispo <aluiz.x6@gmail.com>
2022-08-10 23:34:27 -0300

    listo

---
+++ modulo4/intro-typescript/src/exercicio2.ts
@@ -0,0 +1,8 @@
+function imprimeTresCoresFavoritas(): Any {
+    const cor1: string = [2]
+    const cor2: string = [3]
+    const cor3: string = [4]
+    console.log([cor1, cor2, cor3]) 
+ }
+ 
+ imprimeTresCoresFavoritas()
