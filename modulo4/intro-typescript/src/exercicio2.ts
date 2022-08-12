2f84e573fbb376d28a5f8b2d6ea766c56d69caee
André Luiz Amaral de Oliveira Bispo <aluiz.x6@gmail.com>
2022-08-11 23:03:35 -0300

    feito

--- modulo4/intro-typescript/src/exercicio2.ts
+++ modulo4/intro-typescript/src/exercicio2.ts
@@ -1,7 +1,7 @@
-function imprimeTresCoresFavoritas(): Any {
-    const cor1: string = [2]
-    const cor2: string = [3]
-    const cor3: string = [4]
+function imprimeTresCoresFavoritas(): void {
+    const cor1: string = "amarelo"
+    const cor2: string = "[3]"
+    const cor3: string = "[4]"
     console.log([cor1, cor2, cor3]) 
  }
  
